fedd8846e28648bbc65531f837241255aec8c356
lua <lua@gnomo.net>
2025-04-28 00:07:37 +0200

    chore: added links to the api and webpage when starting the server

--- projecte/api/server.js
+++ projecte/api/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import {routerApi, routerViews} from './routes/router.js';
+import chalk from 'chalk';
 
 const PORT = 3010
-const app = express ();
+const app = express();
 
 
 app.use(express.json());
 
-app.use(express.static("public"));//carpeta publica pel css
+app.use(express.static("public")); //carpeta publica pel css
 app.set('view engine','ejs'); //Fem servir el motor ejs
 app.set('views', './views'); //carpeta on desem els arxius .ejs
 
@@ -16,7 +17,9 @@ app.use('/', routerViews);
 
 
 app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
+    console.log(chalk.green("Server Listening on PORT:"), chalk.blue(PORT));
+    console.log(chalk.yellow(`Api status endpoint: `) + chalk.cyan(`http://localhost:${PORT}/api/status`));
+    console.log(chalk.yellow(`Web endpoint: `) + chalk.cyan(`http://localhost:${PORT}/`));
     }
 );
 
